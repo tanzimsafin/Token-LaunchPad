73274d99961590f97feb35bdf8ee4a1c8dbbd301
tanzim_safin <tanzimhossainsafin756@gmail.com>
2024-10-25 09:22:17 +0600

    done all

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,44 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import "./App.css";
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
+import React from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
-  )
+    <div>
+      <ConnectionProvider
+        endpoint={
+          "https://solana-devnet.g.alchemy.com/v2/cHoM2bnzBlP2k9MAYnYPtBDAzaEjJqnK"
+        }
+      >
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: 20,
+              }}
+            >
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+            <TokenLaunchpad></TokenLaunchpad>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
